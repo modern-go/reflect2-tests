d5f5f1e0b4597e2478205cb5dcb30294c96ffc43
Tao Wen <taowen@gmail.com>
2018-07-01 10:31:43 +0800

    fix tests

---
+++ init.go
@@ -0,0 +1,3 @@
+package reflect2_tests
+
+// dummy
--- type_map_test.go
+++ type_map_test.go
@@ -1,8 +1,9 @@
-package reflect2_test
+package reflect2_tests
 
 import (
 	"github.com/modern-go/reflect2"
 	"testing"
+	"fmt"
 )
 
 type MyStruct struct {
@@ -10,12 +11,13 @@ type MyStruct struct {
 
 func TestTypeByName(t *testing.T) {
 	typByPtr := reflect2.TypeOfPtr((*MyStruct)(nil)).Elem()
-	typByName := reflect2.TypeByName("reflect2_test.MyStruct")
+	typByName := reflect2.TypeByName("reflect2_tests.MyStruct")
 	if typByName != typByPtr {
+		fmt.Println("!!!")
 		t.Fail()
 	}
 	typByPkg := reflect2.TypeByPackageName(
-		"github.com/modern-go/reflect2_test",
+		"github.com/modern-go/reflect2-tests",
 		"MyStruct")
 	if typByPkg != typByPtr {
 		t.Fail()
