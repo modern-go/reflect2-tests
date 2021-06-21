fcffce3cf6d2e92b08e00b50277ce6d5c7846a65
Noah <lh_1989@126.com>
2021-06-21 15:11:06 +0800

    add test case for reflect2.TypesInPackage

--- type_map_test.go
+++ type_map_test.go
@@ -3,23 +3,34 @@ package reflect2_tests
 import (
 	"github.com/modern-go/reflect2"
 	"testing"
-	"fmt"
 )
 
 type MyStruct struct {
 }
 
+var typByPtr = reflect2.TypeOfPtr((*MyStruct)(nil)).Elem()
+
 func TestTypeByName(t *testing.T) {
-	typByPtr := reflect2.TypeOfPtr((*MyStruct)(nil)).Elem()
 	typByName := reflect2.TypeByName("reflect2_tests.MyStruct")
 	if typByName != typByPtr {
-		fmt.Println("!!!")
 		t.Fail()
 	}
+}
+
+func TestTypeByPackageName(t *testing.T) {
 	typByPkg := reflect2.TypeByPackageName(
 		"github.com/modern-go/reflect2-tests",
 		"MyStruct")
 	if typByPkg != typByPtr {
 		t.Fail()
+	}
+}
+
+func TestTypesInPackage(t *testing.T) {
+	typInPkg := reflect2.TypesInPackage("github.com/modern-go/reflect2-tests")
+	for key, typ := range typInPkg {
+		if key != "MyStruct" || typ != typByPtr {
+			t.Fail()
+		}
 	}
 }
